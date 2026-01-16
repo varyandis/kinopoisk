b94bc5a4ef932919f8a4dc99102fd4ef83e21749
Alexandra <varyandis@yandex.ru>
2026-01-16 13:16:54 +0300

    add 404 page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,11 +1,16 @@
-type Props = {
-  
-}
+import { NavLink } from 'react-router'
+import { Button } from 'antd'
+import s from './NotFoundPage.module.css'
 
-export const NotFoundPage = (props: Props) => {
+export const NotFoundPage = () => {
   return (
-    <>
-      NotFoundPage component
-    </>
+    <div className={s.container}>
+      <h1>404</h1>
+      <p>Page not found. We can’t find what you’re looking for</p>
+
+      <Button type="primary">
+        <NavLink to="/">Back to main page</NavLink>
+      </Button>
+    </div>
   )
 }
